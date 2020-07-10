95a5a3db54e1b9e0a81eddde8887d7d19cf03b8f
Angel Zheng <angelz@umich.edu>
2020-07-10 18:22:25 -0400

    Health Bar UI

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -99,7 +99,7 @@ public class RubyController : MonoBehaviour
         }
 
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log(currentHealth + "/" + maxHealth);
+        UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
         animator.SetTrigger("Hit");
     }
 
---
+++ Assets/Scripts/UIHealthBar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIHealthBar : MonoBehaviour
+{
+
+    public static UIHealthBar instance { get; private set; }
+    public UnityEngine.UI.Image mask;
+    float originalsize;
+
+    //executes on instance of object
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalsize = mask.rectTransform.rect.width;
+    }
+
+    public void SetValue(float value)
+    {
+        mask.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal,
+            originalsize * value);
+    }
+}
